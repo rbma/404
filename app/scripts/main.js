6b1b5150ebc3d7434ccffe0c6d9d1cddbea8019a
James Pants <jamespants@gmail.com>
2015-04-20 15:48:42 +0200

    first

---
+++ app/scripts/main.js
@@ -0,0 +1,156 @@
+
+
+'use strict';
+/*global THREE:false */
+
+// -------------------------------------------------
+//
+// Globals
+// 
+// -------------------------------------------------
+
+var container;
+var camera, scene, renderer;
+var mesh, geometry, material;
+
+var mouseX = 0;
+var mouseY = 0;
+var start_time = Date.now();
+
+var windowHalfX = window.innerWidth / 2;
+var windowHalfY = window.innerHeight / 2;
+
+
+
+function init() {
+
+	container = document.createElement( 'div' );
+	document.body.appendChild( container );
+
+	// ------------------------------------------------
+	// Background gradient
+	//
+	
+
+	var canvas = document.createElement( 'canvas' );
+	canvas.width = 32;
+	canvas.height = window.innerHeight;
+
+	var context = canvas.getContext( '2d' );
+
+	var gradient = context.createLinearGradient( 0, 0, 0, canvas.height );
+	gradient.addColorStop(0, '#1e4877');
+	gradient.addColorStop(0.5, '#4584b4');
+
+	context.fillStyle = gradient;
+	context.fillRect(0, 0, canvas.width, canvas.height);
+
+	container.style.background = 'url(' + canvas.toDataURL('image/png') + ')';
+	container.style.backgroundSize = '32px 100%';
+
+	//
+
+	camera = new THREE.PerspectiveCamera( 30, window.innerWidth / window.innerHeight, 1, 3000 );
+	camera.position.z = 6000;
+
+	scene = new THREE.Scene();
+
+	geometry = new THREE.Geometry();
+
+	var texture = THREE.ImageUtils.loadTexture( 'images/cloud.png', null, animate );
+	texture.magFilter = THREE.LinearMipMapLinearFilter;
+	texture.minFilter = THREE.LinearMipMapLinearFilter;
+
+	var fog = new THREE.Fog( 0x4584b4, - 100, 3000 );
+
+	material = new THREE.ShaderMaterial( {
+
+		uniforms: {
+
+			'map': { type: 't', value: texture },
+			'fogColor' : { type: 'c', value: fog.color },
+			'fogNear' : { type: 'f', value: fog.near },
+			'fogFar' : { type: 'f', value: fog.far },
+
+		},
+		vertexShader: document.getElementById( 'vertex' ).textContent,
+		fragmentShader: document.getElementById( 'fragment' ).textContent,
+		transparent: true
+
+	} );
+
+
+	var bufferGeometry = new THREE.PlaneGeometry(64,64);
+	var plane = new THREE.Mesh(bufferGeometry);
+
+	for ( var i = 0; i < 5000; i++ ) {
+
+		plane.position.x = Math.random() * 1000 - 500;
+		plane.position.y = - Math.random() * Math.random() * 200 - 15;
+		plane.position.z = i;
+		plane.rotation.z = Math.random() * Math.PI;
+		plane.scale.x = plane.scale.y = Math.random() * Math.random() * 1.5 + 0.5;
+
+		plane.updateMatrix();
+		geometry.merge(plane.geometry, plane.matrix);
+
+	}
+
+	mesh = new THREE.Mesh( geometry, material );
+	scene.add( mesh );
+
+	mesh = new THREE.Mesh( geometry, material );
+	mesh.position.z = - 5000;
+	scene.add( mesh );
+
+	
+
+	// ------------------------------------------------
+	// Set up renderer
+	//
+	
+	renderer = new THREE.WebGLRenderer( { antialias: false, alpha: true } );
+	renderer.setSize( window.innerWidth, window.innerHeight );
+	container.appendChild( renderer.domElement );
+
+
+	// ------------------------------------------------
+	// Add binds
+	//
+	
+	document.addEventListener( 'mousemove', onDocumentMouseMove, false );
+	window.addEventListener( 'resize', onWindowResize, false );
+
+}
+
+function onDocumentMouseMove( event ) {
+
+	mouseX = ( event.clientX - windowHalfX ) * 0.25;
+	mouseY = ( event.clientY - windowHalfY ) * 0.15;
+
+}
+
+function onWindowResize( event ) {
+
+	camera.aspect = window.innerWidth / window.innerHeight;
+	camera.updateProjectionMatrix();
+
+	renderer.setSize( window.innerWidth, window.innerHeight );
+
+}
+
+function animate() {
+
+	requestAnimationFrame( animate );
+
+	var position = ( ( Date.now() - start_time ) * 0.03 ) % 5000;
+
+	camera.position.x += ( mouseX - camera.position.x ) * 0.01;
+	camera.position.y += ( - mouseY - camera.position.y ) * 0.01;
+	camera.position.z = - position + 5000;
+
+	renderer.render( scene, camera );
+
+}
+
+init();
